5d299713cef529f181e27cfbc7ac1fde72e5a406
Filip <filszab@gmail.com>
2019-06-04 10:34:39 +0200

    poprawa slidera

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,22 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+// document.addEventListener("DOMContentLoaded", function(event) {
+//
+//     let list = document.getElementById("dropdown");
+//     let link = document.getElementById("drop");
+//
+//     link.addEventListener("mouseover" ,function(){
+//       list.style.display = "flex";
+//     });
+//     list.addEventListener("mouseleave" ,function(){
+//       list.style.display = "none";
+//     });
+//
+// });
 
-    let list = document.getElementById("dropdown");
-    let link = document.getElementById("drop");
-
-    link.addEventListener("mouseover" ,function(){
-      list.style.display = "flex";
-    });
-    list.addEventListener("mouseleave" ,function(){
-      list.style.display = "none";
-    });
-
+$("#drop")
+  .on("mouseenter", function() {
+  $("#dropdown").show(300);
+});
+$("#drop")
+  .on("mouseleave", function() {
+  $("#dropdown").hide(300);
 });
