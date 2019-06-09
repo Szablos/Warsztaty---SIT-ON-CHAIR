bdd69a46a297a4f3697ec765fab1f03ec45aefb2
Filip <filszab@gmail.com>
2019-06-10 01:28:30 +0200

    RWD skończone

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,9 @@
 // document.addEventListener("DOMContentLoaded", function(event) {
 //
-//     let list = document.getElementById("dropdown");
-//     let link = document.getElementById("drop");
+//     let list = document.getElementById("dropdown2");
+//     let link = document.getElementById("drop2");
 //
-//     link.addEventListener("mouseover" ,function(){
+//     link.addEventListener("click" ,function(){
 //       list.style.display = "flex";
 //     });
 //     list.addEventListener("mouseleave" ,function(){
@@ -11,12 +11,31 @@
 //     });
 //
 // });
+$(() => {
+  let btnTog = $('.header__navigation').children('button');
+  let tog = $('.toggler');
 
-$("#drop")
-  .on("mouseenter", function() {
-  $("#dropdown").show(300);
-});
-$("#drop")
-  .on("mouseleave", function() {
-  $("#dropdown").hide(300);
-});
+  $("#drop")
+    .on("mouseenter", function() {
+    $("#dropdown").show(300);
+  });
+  $("#drop")
+    .on("mouseleave", function() {
+    $("#dropdown").hide(300);
+  });
+
+  $("#drop2")
+    .on("mouseenter", function() {
+    $("#dropdown2").show(300);
+  });
+  $("#drop2")
+    .on("mouseleave", function() {
+    $("#dropdown2").hide(300);
+  });
+
+  btnTog.on("click", function() {
+    tog.toggle("slow");
+  })
+
+
+})
